c70966e2555dfd099d14fa95ece27d62b674c7ff
Angie Yague <angietoledanovillalba@gmail.com>
2026-01-12 11:32:51 +0000

    Rate Limit config

--- Backend/middlewares/rateLimit.js
+++ Backend/middlewares/rateLimit.js
@@ -2,6 +2,6 @@ const rateLimit = require("express-rate-limit");
 
 module.exports = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 500,
     message: { error: "Demasiadas peticiones desde esta IP." }
 });
