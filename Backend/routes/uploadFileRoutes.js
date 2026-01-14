e3d59d6b9bdd39ca077de2d5331836c31d59269c
Angie Yague <angietoledanovillalba@gmail.com>
2026-01-14 10:24:21 +0000

    Fix ruta upload limiter

--- Backend/routes/uploadFileRoutes.js
+++ Backend/routes/uploadFileRoutes.js
@@ -1,6 +1,6 @@
 const express= require('express');
 require('dotenv').config();
-const {uploadLimiter} = require("../middlewares/fileLimiter");
+const uploadLimiter = require("../middlewares/fileLimiter");
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
