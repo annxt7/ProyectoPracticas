35aadfc1718504ce7ea930a621973f30db19ca99
Angie Yague <angietoledanovillalba@gmail.com>
2026-01-14 09:46:39 +0000

    Optimización de imgs antes de subir al server 2

--- Frontend/src/services/upload.js
+++ Frontend/src/services/upload.js
@@ -1,26 +1,32 @@
-import api from "./api"; // Tu instancia de axios que apunta a localhost:3000/api
+import api from "./api";
+import imageCompression from 'browser-image-compression'; // <--- IMPORTANTE
 
 export const uploadFileToCloudinary = async (file) => {
   if (!file) return null;
 
-  const formData = new FormData();
-  // "imagen" debe coincidir con upload.single('imagen') de tu backend
-  formData.append("imagen", file); 
-
   try {
-    // IMPORTANTE: La ruta es /files/upload 
-    // (porque en server.js pusimos /api/files y en el router pusimos /upload)
+   
+    const options = {
+      maxSizeMB: 0.8,          
+      maxWidthOrHeight: 1200, 
+      useWebWorker: true,
+    };
+    const compressedFile = await imageCompression(file, options);
+
+    // 3. ENVIAMOS EL ARCHIVO COMPRIMIDO
+    const formData = new FormData();
+    formData.append("imagen", compressedFile); // <--- Enviamos el comprimido
+
     const response = await api.post("/files/upload", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     });
 
-    // Tu backend devuelve: { success: true, url: "..." }
     return response.data.url; 
     
   } catch (error) {
-    console.error("Error subiendo imagen:", error);
+    console.error("Error en el proceso (compresión o subida):", error);
     throw error;
   }
 };
