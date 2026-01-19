18257cf033a0e6e3dc31875d5acab8c8951b6575
Angie Yague <angietoledanovillalba@gmail.com>
2026-01-19 11:15:51 +0000

    Fix error sql

--- Backend/controllers/searchController.js
+++ Backend/controllers/searchController.js
@@ -17,7 +17,7 @@ exports.searchTribe = async (req, res, next) => {
                 CONCAT('@', username) AS handle, 
                 bio, 
                 avatar_url AS img,
-                role
+                role,
                 FALSE AS isFollowing 
             FROM Users 
             WHERE username LIKE ? 
