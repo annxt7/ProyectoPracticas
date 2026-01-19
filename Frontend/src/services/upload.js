26d0022de21216900f2fa3cadeb7069ecc0ce679
Angie Yague <angietoledanovillalba@gmail.com>
2026-01-19 09:25:44 +0000

    intentando arreglar colecciones

--- Frontend/src/services/upload.js
+++ Frontend/src/services/upload.js
@@ -1,39 +1,33 @@
-import api from "./api";
-import imageCompression from 'browser-image-compression';
 
-export const uploadFileToCloudinary = async (file) => {
-  if (!file) return null;
+const express= require('express');
+require('dotenv').config();
+const uploadLimiter = require("../middlewares/fileLimiter");
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const router = express.Router();
+const { verifyToken } = require("../middlewares/authMiddleware");
 
-  try {
-    const options = {
-      maxSizeMB: 0.5,
-      maxWidthOrHeight: 1200,
-      useWebWorker: true,
-      // Mantenemos el tipo original
-    };
-    
-    // 1. Comprimimos (devuelve un Blob)
-    const compressedBlob = await imageCompression(file, options);
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'mis-colecciones', 
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+  },
+});
 
-    // 2. CONVERSIÓN CRÍTICA: De Blob a File
-    // Esto asegura que el servidor reciba un archivo con nombre, tipo y fecha.
-    const compressedFile = new File([compressedBlob], file.name, {
-      type: file.type,
-      lastModified: Date.now(),
-    });
+const upload = multer({ storage: storage });
 
-    const formData = new FormData();
-    // 3. Enviamos el File real
-    formData.append("imagen", compressedFile); 
+//POST:
+router.post('/upload',verifyToken,uploadLimiter,upload.single('imagen'),(req,res)=>{
+try{
+res.json({
+    success:true,
+    url:req.file.path
+});
+}catch(error){
+res.status(500).json({success:false, error: error.message});
+}
+});
 
-    const response = await api.post("/files/upload", formData, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-
-    // Importante: Verifica que tu backend devuelva exactamente la propiedad 'url'
-    return response.data.url; 
-  } catch (error) {
-    console.error("Error en uploadFileToCloudinary:", error);
-    throw error;
-  }
-};
+module.exports=router
