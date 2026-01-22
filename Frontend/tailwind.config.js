a2ffc47e2675db9c30d795a3acab81815ee43fcf
Axel <axelgonzalezp82@gmail.com>
2026-01-22 10:00:13 +0100

    Cambios tailwind

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,10 +1,19 @@
 // tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  // 1. Añade esta sección de content:
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
 
   plugins: [require("daisyui")],
-  
+
   daisyui: {
-    themes: ["lofi", "black"], // "lofi" para claro minimalista, "black" para oscuro puro
+    themes: ["lofi", "black"], 
   },
 }
