29998f2b34dfa49bd8b1d47e6aa026e870db55de
Angie Yague <angietoledanovillalba@gmail.com>
2026-01-22 11:28:48 +0000

    Temas 3

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "synthwave", "retro", "aqua"],
+  },
+  plugins: [],
+}
