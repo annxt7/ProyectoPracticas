6965ad0a49cc9781f0b17aa3014a130d238700a6
Axel <axelgonzalezp82@gmail.com>
2026-01-30 10:42:20 +0100

    Letra landing

--- Frontend/src/pages/Buscador.jsx
+++ Frontend/src/pages/Buscador.jsx
@@ -211,7 +211,7 @@ const Explorer = () => {
                     <h3 className="font-bold truncate text-[13px] group-hover:text-primary transition-colors">
                       {col.title}
                     </h3>
-                    <p className="text-[10px] text-primary font-extrabold">
+                    <p className="text-[10px] text-primary font-bold uppercase">
                       @{col.author}
                     </p>
                   </div>
